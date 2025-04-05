aeea990ee81289dadf5221b7872b9bea2f4eabcb
siwam <siwamsingh2003@gmail.com>
2025-04-05 14:35:24 +0530

    added not-found.tsx

---
+++ client/app/not-found.tsx
@@ -0,0 +1,12 @@
+import NotFoundPage from '@/components/Error/NotFound'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <NotFoundPage/>
+    </div>
+  )
+}
+
+export default NotFound
